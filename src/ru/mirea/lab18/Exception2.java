ab36f9a870b6c669acb6ab8addafe1da1941848c
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2023-10-15 16:14:29 +0300

    18 практическая

---
+++ src/ru/mirea/lab18/Exception2.java
@@ -0,0 +1,28 @@
+package ru.mirea.lab18;
+
+import java.util.Scanner;
+
+public class Exception2 {
+    public static void main(String[] args) {
+        exceptionDemo();
+    }
+    public static void exceptionDemo() {
+        Scanner myScanner = new Scanner(System.in);
+        System.out.print("Введите число: ");
+        String intString = myScanner.next();
+        try {
+            int i = Integer.parseInt(intString);
+            System.out.println(2 / i);
+        } catch (NumberFormatException e) {
+            System.out.println("Введено не число: " + e.getMessage());
+        } catch (ArithmeticException e) {
+            System.out.println("Деление на 0 запрещено: " + e.getMessage());
+        } catch (Exception e) {
+            // обработка других видов исключений
+            System.out.println("Произошла ошибка: " + e.getMessage());
+        } finally {
+            myScanner.close(); // Закрываем сканнер, чтобы освободить ресурсы
+        }
+    }
+
+}
