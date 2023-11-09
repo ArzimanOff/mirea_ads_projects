be86af28fea152c155d103677aab19ad5c1645bb
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2023-11-09 20:59:13 +0300

    20 практическая

--- src/ru/mirea/lab_20/ThreeTypesGenericTester.java
+++ src/ru/mirea/lab_20/ThreeTypesGenericTester.java
@@ -2,7 +2,7 @@ package ru.mirea.lab_20;
 
 public class ThreeTypesGenericTester {
     public static void main(String[] args) {
-        ThreeGeneric<String, Dog, Integer> person = new ThreeGeneric<>("Шарик", new Dog(), 10);
-        person.showTypes();
+        ThreeGeneric<String, Dog, Integer> dog = new ThreeGeneric<>("Шарик", new Dog(), 10);
+        dog.showTypes();
     }
 }
