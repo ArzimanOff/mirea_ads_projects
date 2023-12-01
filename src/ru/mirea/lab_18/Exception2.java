7e870402aad2ea5e829a806f69ea2a4d0f979808
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2023-12-01 09:37:11 +0300

    some fix, with scanner

--- src/ru/mirea/lab_18/Exception2.java
+++ src/ru/mirea/lab_18/Exception2.java
@@ -9,12 +9,13 @@ public class Exception2 {
     public static void exceptionDemo() {
         Scanner scanner = new Scanner(System.in);
         System.out.print("Введите число: ");
-        String intString = scanner.next();
+
         try {
+            String intString = scanner.next();
             int i = Integer.parseInt(intString);
-            System.out.println(2 / i);
+            System.out.println(12 / i);
         } catch (NumberFormatException e) {
-            System.out.println("Введено не число: " + e.getMessage());
+            System.out.println("Введено не число типа Int : " + e.getMessage());
         } catch (ArithmeticException e) {
             System.out.println("Деление на 0 запрещено: " + e.getMessage());
         } catch (Exception e) {
--- src/ru/mirea/lab_19/task1/InnNumTester.java
+++ src/ru/mirea/lab_19/task1/InnNumTester.java
@@ -3,26 +3,30 @@ package ru.mirea.lab_19.task1;
 import org.jetbrains.annotations.NotNull;
 
 import java.math.BigInteger;
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class InnNumTester {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        while(true){
-            System.out.println("Введите номер ИНН: ");
-            try{
-                BigInteger innNum =  new BigInteger(scanner.nextLine().trim());
-                checkInn(innNum);
-                break;
-            }catch (InnNotValidException e){
-                System.out.println(e.getLocalizedMessage());
-            } catch (Exception e) {
-                // обработка других видов исключений
-                System.out.println("Ошибка ввода ИНН номера: " + e.getMessage() + " -> присутствуют символы помимо цифр");
+        try (Scanner scanner = new Scanner(System.in)) {
+            while (true) {
+                System.out.println("Введите номер ИНН: ");
+                try {
+                    BigInteger innNum = new BigInteger(scanner.nextLine().trim());
+                    checkInn(innNum);
+                    System.out.println("Верный ИНН!");
+                    break;
+                } catch (InnNotValidException e) {
+                    System.out.println(e.getLocalizedMessage());
+                } catch (InputMismatchException e) {
+                    System.out.println("Ошибка ввода ИНН номера: " + e.getMessage() + " -> присутствуют символы помимо цифр");
+                    // Очистите буфер после ошибочного ввода
+                    scanner.nextLine();
+                } catch (Exception e) {
+                    System.out.println("Произошла непредвиденная ошибка: " + e.getMessage());
+                }
             }
         }
-        scanner.close();
-        System.out.println("Верный ИНН!");
     }
 
     public static void checkInn(@NotNull BigInteger innNum) throws InnNotValidException{
