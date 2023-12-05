08feb210088708f0adb37cab3ce80641f7d53241
ArzzzimanOff <arzimanovgyulahmed@yandex.ru>
2023-12-05 20:48:44 +0300

    Исправлен баг с повторным использованием старого стека для новой последовательности.
    + добавлены новые тест-кейсы
    + добавлена обработка последовательности null

--- src/ru/mirea/bonus_task/BracketSequence.java
+++ src/ru/mirea/bonus_task/BracketSequence.java
@@ -3,16 +3,19 @@ package ru.mirea.bonus_task;
 import org.jetbrains.annotations.NotNull;
 
 public class BracketSequence {
-    private String exp;
-    private boolean isBalanced;
-    private static MyStack<Character> stack = new MyStack<>();
+    private final String exp;
+    private final boolean isBalanced;
 
     public BracketSequence(String exp) {
         this.exp = exp;
         this.isBalanced = checkBalance(this.exp);
     }
 
-    public static boolean checkBalance(@NotNull String exp) {
+    public static boolean checkBalance(String exp) {
+        MyStack<Character> stack = new MyStack<>();
+        if (exp == null) {
+            throw new IllegalArgumentException("Скобочная последовательность не может быть равна null !");
+        }
         for (char bracket : exp.toCharArray()) {
             if (bracket == '(' || bracket == '{' || bracket == '[') {
                 stack.push(bracket);
@@ -29,10 +32,6 @@ public class BracketSequence {
         return (opening == '(' && closing == ')') ||
                 (opening == '{' && closing == '}') ||
                 (opening == '[' && closing == ']');
-    }
-
-    public String getExp() {
-        return exp;
     }
 
     public boolean isBalanced() {
--- src/ru/mirea/bonus_task/TaskTester.java
+++ src/ru/mirea/bonus_task/TaskTester.java
@@ -4,11 +4,21 @@ public class TaskTester {
     public static void main(String[] args) {
         String exp1 = "[()]{}{[()()]()}";
         String exp2 = "[(])";
+        String exp3 = "([{}])";
+        String exp4 = "";
+        //String exp5 = null;
+
         BracketSequence test1 = new BracketSequence(exp1);
         BracketSequence test2 = new BracketSequence(exp2);
+        BracketSequence test3 = new BracketSequence(exp3);
+        BracketSequence test4 = new BracketSequence(exp4);
+        //BracketSequence test5 = new BracketSequence(exp5);
 
 
-        System.out.println("Expression 1 is balanced: " + test1.isBalanced());
-        System.out.println("Expression 2 is balanced: " + test2.isBalanced());
+        System.out.println("Последовательность 1: " + test1.isBalanced());
+        System.out.println("Последовательность 2: " + test2.isBalanced());
+        System.out.println("Последовательность 3: " + test3.isBalanced());
+        System.out.println("Последовательность 4: " + test4.isBalanced());
+        //System.out.println("Последовательность 5: " + test5.isBalanced());
     }
 }
